7d3489e9acfc9819c9e44ffe959a58f5ca8f41d8
rwill33 <90706510+rwill33@users.noreply.github.com>
2022-11-10 22:06:14 -0500

    Adds firebase auth service follows https://www.positronx.io/full-angular-firebase-authentication-system/

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,20 +4,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from 'src/environments/environment';
+
+// Auth Service
+import { AuthService } from "./shared/services/auth.service";
+
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
     TabsModule.forRoot(),
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCTLGFrqn1NOrtUQMN7WITJBSve7vrj7VQ",
+    authDomain: "rwill33-lab4.firebaseapp.com",
+    projectId: "rwill33-lab4",
+    storageBucket: "rwill33-lab4.appspot.com",
+    messagingSenderId: "724042200276",
+    appId: "1:724042200276:web:f0315b5a4fb13260cc3741",
+    measurementId: "G-J6E5PBL3P9"
+  }
 };
 
 /*

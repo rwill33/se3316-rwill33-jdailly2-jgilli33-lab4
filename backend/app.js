3330d90f51a30bdb5dcfc4bbca75c48d0f0fc142
rwill33 <90706510+rwill33@users.noreply.github.com>
2022-11-29 13:17:26 -0500

    Fixes issue with updating playlist

--- backend/app.js
+++ backend/app.js
@@ -263,8 +263,17 @@ router.route('/playlists')
   })
   // Add to playlist
   .post(async (req, res) => {
-    if (req.body.isPublic){
-      connection.query(`UPDATE UserPlaylists SET isPublic=${req.body.isPublic} WHERE playlistId='${req.body.playlistId}'`, (err, rows, fields) => {
+    console.log(req.body);
+    if(req.body.description && req.body.name){
+      connection.query(`UPDATE UserPlaylists SET playlistName='${req.body.name}', description='${req.body.description}' WHERE playlistId=${req.body.playlistId}`, (err, rows, fields) => {
+        if (err) {
+          res.status(500).send(`Error Updating Playlist.`);
+        } else {
+          res.send(rows);
+        }
+      });
+    } else if(req.body.name) {
+      connection.query(`UPDATE UserPlaylists SET playlistName='${req.body.name}', description=null WHERE playlistId=${req.body.playlistId}`, (err, rows, fields) => {
         if (err) {
           res.status(500).send(`Error Updating Playlist.`);
         } else {
@@ -272,25 +281,14 @@ router.route('/playlists')
         }
       });
     } else {
-      if(req.body.description) {
-        connection.query(`UPDATE UserPlaylists SET playlistName='${req.body.name}', description='${req.body.description}' WHERE playlistId=${req.body.playlistId}`, (err, rows, fields) => {
-          if (err) {
-            res.status(500).send(`Error Updating Playlist.`);
-          } else {
-            res.send(rows);
-          }
-        });
-      } else {
-        connection.query(`UPDATE UserPlaylists SET playlistName='${req.body.name}', description=null WHERE playlistId=${req.body.playlistId}`, (err, rows, fields) => {
-          if (err) {
-            res.status(500).send(`Error Updating Playlist.`);
-          } else {
-            res.send(rows);
-          }
-        });
-      }
+      connection.query(`UPDATE UserPlaylists SET isPublic=${req.body.isPublic} WHERE playlistId='${req.body.playlistId}'`, (err, rows, fields) => {
+        if (err) {
+          res.status(500).send(`Error Updating Playlist.`);
+        } else {
+          res.send(rows);
+        }
+      });
     }
-
   })
   // Delete Playlist
   .delete(async (req, res) => {

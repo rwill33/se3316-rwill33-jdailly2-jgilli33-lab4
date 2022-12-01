72dda284d8613a628b4e2da62052d80174bf9bcf
rwill33 <90706510+rwill33@users.noreply.github.com>
2022-11-30 22:59:43 -0500

    Add production build

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCTLGFrqn1NOrtUQMN7WITJBSve7vrj7VQ",
+    authDomain: "rwill33-lab4.firebaseapp.com",
+    projectId: "rwill33-lab4",
+    storageBucket: "rwill33-lab4.appspot.com",
+    messagingSenderId: "724042200276",
+    appId: "1:724042200276:web:f0315b5a4fb13260cc3741",
+    measurementId: "G-J6E5PBL3P9"
+  }
 };

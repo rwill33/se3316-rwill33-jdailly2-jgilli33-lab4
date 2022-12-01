5dce7c65253836ba47e4ab6458857eb7433f889b
rwill33 <90706510+rwill33@users.noreply.github.com>
2022-11-30 23:14:31 -0500

    Change domain

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ const adminApp = admin.initializeApp({
 
 
 var corsOptions = {
-  origin: 'http://localhost:4200',
+  origin: 'https://ronin.software',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
 
